2a6502df10687a0c337850b6fe7732d896d6a680
guillaume naimi <guillaume.naimi@hipli.fr>
2026-04-13 11:16:52 +0200

    fix: FIx build with tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
-import { devtools } from '@tanstack/devtools-vite';
-import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import viteReact from '@vitejs/plugin-react';
-import { codeInspectorPlugin } from 'code-inspector-plugin';
-import cpy from 'cpy';
-import { Nitro } from 'nitro/types';
-import { nitro } from 'nitro/vite';
-import { resolve } from 'node:path';
-import { defineConfig, loadEnv } from 'vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import cpy from "cpy";
+import { Nitro } from "nitro/types";
+import { nitro } from "nitro/vite";
+import { resolve } from "node:path";
+import { defineConfig, loadEnv } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 const { nitroRetrieveServerDirHook, prismaCopyBinariesPlugin } =
   createPrismaCopyBinariesPlugin();
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
+  const env = loadEnv(mode, process.cwd(), "VITE_");
   return {
     server: {
       port: env.VITE_PORT ? Number(env.VITE_PORT) : 3000,
@@ -22,19 +20,18 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       devtools(),
-      codeInspectorPlugin({
-        bundler: 'vite',
-      }),
       tsConfigPaths(),
       tanstackStart(),
       nitro({
-        hooks: {
-          'build:before': nitroRetrieveServerDirHook,
-        },
-        routeRules: { '/storybook': { redirect: '/storybook/index.html' } },
+        modules: [
+          (nitro) => {
+            nitro.hooks.hook("build:before", () => {
+              nitroRetrieveServerDirHook(nitro);
+            });
+          },
+        ],
+        routeRules: { "/storybook": { redirect: "/storybook/" } },
       }),
-      // react's vite plugin must come after start's vite plugin
-      viteReact({}),
       // Copy prisma binaries at the end
       prismaCopyBinariesPlugin(),
     ],
@@ -42,20 +39,17 @@ export default defineConfig(({ mode }) => {
 });
 
 function createPrismaCopyBinariesPlugin() {
-  let serverDir = '';
+  let serverDir = "";
   return {
     nitroRetrieveServerDirHook: (nitro: Nitro) => {
-      serverDir = nitro.options.output.serverDir.replace(resolve('.'), '.');
+      serverDir = nitro.options.output.serverDir.replace(resolve("."), ".");
     },
     prismaCopyBinariesPlugin: () => ({
-      name: 'prisma-copy-binaries',
+      name: "prisma-copy-binaries",
       writeBundle: async (outputOptions: { dir?: string }) => {
-        const outputDir = outputOptions.dir?.replace(resolve('.'), '.');
+        const outputDir = outputOptions.dir?.replace(resolve("."), ".");
         if (outputDir === serverDir) {
-          await cpy(
-            './src/server/db/generated/**/*.node',
-            resolve(serverDir, 'chunks')
-          );
+          await cpy("./src/server/db/generated/**/*.node", resolve(serverDir));
         }
       },
     }),
